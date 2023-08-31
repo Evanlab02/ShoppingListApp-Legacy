42ee0f7e9ddb33bfe2cf019ff729860b9c0839e0
Evanlab02 <Evanlabuschagne70@gmail.com>
2023-08-31 15:35:28 +0200

    feat(Makefile): Added rule to generate static files for django

--- backend/manage.py
+++ backend/manage.py
@@ -8,7 +8,7 @@ from django.core.management import execute_from_command_line
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shoppingapp.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shoppingapp.settings.settings")
     execute_from_command_line(sys.argv)
 
 
