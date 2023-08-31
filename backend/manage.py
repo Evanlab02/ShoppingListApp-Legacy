f4f57767f04e6afa1ec1b118c893bf174259e863
Evanlab02 <Evanlabuschagne70@gmail.com>
2023-08-31 15:03:07 +0200

    feat(Linting): Added linting and linting pipelines

--- backend/manage.py
+++ backend/manage.py
@@ -5,6 +5,7 @@ import sys
 
 from django.core.management import execute_from_command_line
 
+
 def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shoppingapp.settings")
