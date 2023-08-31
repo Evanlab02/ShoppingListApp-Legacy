93d36bf46e18156b2c24dff79168cef80d2e3ac1
Evanlab02 <Evanlabuschagne70@gmail.com>
2023-08-31 17:46:51 +0200

    feat(Django): Development backend created

--- backend/shoppingapp/settings/local_settings.py
+++ backend/shoppingapp/settings/local_settings.py
@@ -30,9 +30,7 @@ SECRET_KEY = environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "localhost"
-]
+ALLOWED_HOSTS = ["localhost"]
 
 
 # Application definition
--- backend/shoppingapp/settings/settings.py
+++ backend/shoppingapp/settings/settings.py
@@ -30,9 +30,7 @@ SECRET_KEY = environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    "localhost"
-]
+ALLOWED_HOSTS = ["localhost"]
 
 
 # Application definition
