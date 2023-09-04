3770cd7b267f482214418f2e307d3d3f52621af5
Evanlab02 <Evanlabuschagne70@gmail.com>
2023-09-04 16:19:44 +0200

    chore(Formatting): Formatted backend files

--- backend/shoppingapp/settings/settings.py
+++ backend/shoppingapp/settings/settings.py
@@ -30,10 +30,7 @@ SECRET_KEY = environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "192.168.0.3"
-]
+ALLOWED_HOSTS = ["localhost", "192.168.0.3"]
 
 
 # Application definition
