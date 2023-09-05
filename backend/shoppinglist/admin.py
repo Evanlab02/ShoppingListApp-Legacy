5c6a49d5c00a8497d3a13861c1f7ea7a251670cc
Evanlab02 <Evanlabuschagne70@gmail.com>
2023-09-05 09:39:19 +0200

    feat(Admin): Added shopping lists to admin page

---
+++ backend/shoppinglist/admin.py
@@ -0,0 +1,14 @@
+"""Configures the admin interface for the shoppinglist app."""
+
+from django.contrib import admin
+
+from .models import ShoppingList
+
+class ShoppingListAdmin(admin.ModelAdmin):
+    """Configures the admin interface for the ShoppingList model."""
+
+    list_display = ("name", "created_at", "updated_at", "is_current")
+    list_filter = ("created_at", "updated_at")
+    search_fields = ("name", "description")
+
+admin.site.register(ShoppingList, ShoppingListAdmin)
---
+++ backend/shoppinglist/apps.py
@@ -0,0 +1,8 @@
+"""Contains the ShoppinglistConfig class."""
+
+from django.apps import AppConfig
+
+
+class ShoppinglistConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "shoppinglist"
