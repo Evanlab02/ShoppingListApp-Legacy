96b76a904f39e2f4c63def8a497108cbde325146
Evanlab02 <Evanlabuschagne70@gmail.com>
2023-09-05 10:58:52 +0200

    feat(ShoppingList): Can now create a shopping list through the API

---
+++ backend/shoppinglist/validation/validation.py
@@ -0,0 +1,24 @@
+"""Contains validation functions for shoppinglist app."""
+
+from ..models import ShoppingList
+
+
+def validate_shoppinglist(shoppinglist: ShoppingList):
+    """Validate the shoppinglist object."""
+    if shoppinglist.start_date > shoppinglist.end_date:
+        raise ValueError("The shopping list's start date must be before its end date.")
+
+    lists = ShoppingList.objects.all()
+    for shopping_list in lists:
+        if shoppinglist.start_date <= shopping_list.start_date <= shoppinglist.end_date:
+            raise ValueError("A shopping list already exists for this date range.")
+        if shoppinglist.start_date <= shopping_list.end_date <= shoppinglist.end_date:
+            raise ValueError("A shopping list already exists for this date range.")
+        if (
+            shopping_list.start_date
+            <= shoppinglist.start_date
+            <= shopping_list.end_date
+        ):
+            raise ValueError("A shopping list already exists for this date range.")
+        if shopping_list.start_date <= shoppinglist.end_date <= shopping_list.end_date:
+            raise ValueError("A shopping list already exists for this date range.")
