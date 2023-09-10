520174c1f7c73f73114f22dbdca53de265c4a755
Evanlab02 <Evanlabuschagne70@gmail.com>
2023-09-10 23:01:15 +0200

    feat(Stores): Created store model and added it to admin page

--- backend/shoppingitem/admin.py
+++ backend/shoppingitem/admin.py
@@ -2,7 +2,7 @@
 
 from django.contrib import admin
 
-from .models import ShoppingItem
+from .models import ShoppingItem, ShoppingStore
 
 class ShoppingItemAdmin(admin.ModelAdmin):
     """Admin configuration for the shoppingitem app."""
@@ -11,4 +11,12 @@ class ShoppingItemAdmin(admin.ModelAdmin):
     search_fields = ('name', 'store')
     list_per_page = 25
 
+class ShoppingStoreAdmin(admin.ModelAdmin):
+    """Admin configuration for the shoppingstore app."""
+    list_display = ('name', 'store_type', 'created_at', 'updated_at')
+    list_display_links = ('name',)
+    search_fields = ('name', 'store_type')
+    list_per_page = 25
+
 admin.site.register(ShoppingItem, ShoppingItemAdmin)
+admin.site.register(ShoppingStore, ShoppingStoreAdmin)
