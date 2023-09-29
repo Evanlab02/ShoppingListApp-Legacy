ed112bcf75666fcf6b877bc1c9720f012619a637
Evanlab02 <evanlabuschagne70@gmail.com>
2023-09-30 00:46:08 +0200

    feat(Budgets): Can now link budgets to shopping lists

--- backend/shoppinglist/models.py
+++ backend/shoppinglist/models.py
@@ -7,7 +7,6 @@ from django.contrib.auth.models import User
 
 from shoppingitem.models import ShoppingItem
 
-
 class ShoppingList(models.Model):
     """Represents a shopping list."""
 
@@ -37,3 +36,9 @@ class ShoppingList(models.Model):
             .filter(end_date__gte=timezone.now().date())
             .first()
         )
+
+class ShoppingBudget(models.Model):
+    """Represents a shopping budget."""
+
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    shopping_list = models.ForeignKey(ShoppingList, on_delete=models.CASCADE)
